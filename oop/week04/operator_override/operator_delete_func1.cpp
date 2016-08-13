3fe905e0e0943c887e556d3e41c4e57aa7ab2f8e
shuailong <shuailong@tenxcloud.com>
2016-08-13 02:10:49 -0700

    add example of operator new and delete

---
+++ oop/week04/operator_override/operator_delete_func1.cpp
@@ -0,0 +1,57 @@
+// spec1_the_operator_delete_function1.cpp
+// compile with: /EHsc
+// arguments: 3
+#include <iostream>
+#include <malloc.h>
+#include <memory.h>
+#include <stdlib.h>
+using namespace std;
+
+int fLogMemory = 0;      // Perform logging (0=no; nonzero=yes)?
+int cBlocksAllocated = 0;  // Count of blocks allocated.
+
+void * operator new[] (size_t) {
+    clog << "new[]\n";
+    return 0;
+}
+void operator delete[] (void*) {
+    clog << "delete[]\n";
+}
+
+// User-defined operator new.
+void *operator new( size_t stAllocateBlock ) {
+   static int fInOpNew = 0;   // Guard flag.
+
+   if ( fLogMemory && !fInOpNew ) {
+      fInOpNew = 1;
+      clog << "Memory block " << ++cBlocksAllocated
+          << " allocated for " << stAllocateBlock
+          << " bytes\n";
+      fInOpNew = 0;
+   }
+   return malloc( stAllocateBlock );
+}
+
+// User-defined operator delete.
+void operator delete( void *pvMem ) {
+   static int fInOpDelete = 0;   // Guard flag.
+   if ( fLogMemory && !fInOpDelete ) {
+      fInOpDelete = 1;
+      clog << "Memory block " << cBlocksAllocated--
+          << " deallocated\n";
+      fInOpDelete = 0;
+   }
+
+   free( pvMem );
+}
+
+int main( int argc, char *argv[] ) {
+   fLogMemory = 1;   // Turn logging on
+   if( argc > 1 )
+      for( int i = 0; i < atoi( argv[1] ); ++i ) {
+         char *pMem = new char[10];
+         delete[] pMem;
+      }
+   fLogMemory = 0;  // Turn logging off.
+   return cBlocksAllocated;
+}
