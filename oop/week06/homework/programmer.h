7dfeebb664c7d6c64e28e8d06901bdf464339a9e
shuailong <shuailong@tenxcloud.com>
2016-08-29 19:17:32 +0800

    some bug fix

--- oop/week06/homework/programmer.h
+++ oop/week06/homework/programmer.h
@@ -6,6 +6,8 @@
 
 using std::wstring;
 
+class ProgrammerIdGreater;
+
 class Programmer {
 private:
   int Id;
@@ -16,10 +18,12 @@ public:
   void Print() const {
     std::wcout << L"[" << Id << L"]:" << Name << std::endl;
   }
+  friend class ProgrammerIdGreater;
 };
 
-class ProgrammerIdGreater : binary_function <Programmer, Programmer, bool> {
+class ProgrammerIdGreater {
   bool operator() (const Programmer& x, const Programmer& y) const {return x.Id > y.Id;}
-}
+};
 
+// http://stackoverflow.com/questions/33114656/replacement-for-binary-function
 #endif
