8531381f1d305b9183b220dbb40b7bb16eebbc2c
oscarzhao <oscarzhaosl@gmail.com>
2016-09-07 01:06:37 +0800

    bugfix for test script, remove CRLF character to match

--- coursera/basic_data_structure/week01/homework/check_brackets_in_code/test.sh
+++ coursera/basic_data_structure/week01/homework/check_brackets_in_code/test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e  # exit with none zero
 
+# test files have ending of CRLF, so need to truncate the string read from them
+# use ${str%?}
+
 # compile the cpp 
 g++ -o main check_brackets.cpp -lm 
 
@@ -9,7 +12,7 @@ for f in ${inputs}
 do
   should=$(cat tests/${f}.a)
   real=$(./main < tests/${f})
-  if [ "${should}" != "${real}" ]; then
+  if [ "${should%?}" != "${real}" ]; then  # use %? to filter out the last character
     echo "tests/${f} -> |${real}||, should be |||${should}|"
     exit 1
   fi
