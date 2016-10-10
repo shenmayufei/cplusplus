7f4d2babc6f796953f95bdb1cd48d8624fe8b70f
oscarzhao <oscarzhaosl@gmail.com>
2016-10-11 00:49:07 +0800

    graphs homework reachability

--- coursera/graphs/week01/homework/reachability/reachability.cpp
+++ coursera/graphs/week01/homework/reachability/reachability.cpp
@@ -4,10 +4,23 @@
 using std::vector;
 using std::pair;
 
+int reach_helper(vector<vector<int> > &adj, vector<bool> &visited, int x, int y) {
+  const vector<int>& vArr = adj[x];
+  for(std::vector<int>::const_iterator it = vArr.begin(); it != vArr.end(); it++) {
+    if (*it == y) return 1;
+  }
+  for(std::vector<int>::const_iterator it = vArr.begin(); it != vArr.end(); it++) {
+    if (visited[y]) continue;
+    visited[y] = true;
+    if (reach_helper(adj, visited, *it, y)) return 1;
+  }
+  return 0;
+}
 
 int reach(vector<vector<int> > &adj, int x, int y) {
   //write your code here
-  return 0;
+  vector<bool> visited(adj.size(), false);
+  return reach_helper(adj, visited, x, y);
 }
 
 int main() {
