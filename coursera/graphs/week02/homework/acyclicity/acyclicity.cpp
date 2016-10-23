61f1e64b13bd9ea56f2ada96bea3e9bd80b86e8a
oscarzhao <oscarzhaosl@gmail.com>
2016-10-23 20:59:03 +0800

    add graph week02 homework toposort

--- coursera/graphs/week02/homework/acyclicity/acyclicity.cpp
+++ coursera/graphs/week02/homework/acyclicity/acyclicity.cpp
@@ -5,6 +5,7 @@ using std::vector;
 using std::pair;
 
 bool acyclic_helper(vector<vector<int> >& adj, int v, int key, vector<bool>& visited) {
+  // std::cout << "v=" << v << " visited[v]=" << visited[v] << ", key=" << key << ", visited[key]=" << visited[key]<< std::endl;
   if (visited[v]) {
     if (key == v) return true;
     else return false;
@@ -15,7 +16,7 @@ bool acyclic_helper(vector<vector<int> >& adj, int v, int key, vector<bool>& vis
 
   vector<int>& vec = adj[v];
   for(int i = 0; i < vec.size(); i++) {
-    if (acyclic_helper(adj, i, key, visited)) return true;
+    if (acyclic_helper(adj, vec[i], key, visited)) return true;
   }
   return false;
 }
@@ -25,9 +26,11 @@ bool acyclic(vector<vector<int> > &adj) {
   for(int i = 0; i < vNum; i++) {
     vector<bool> visited(adj.size(), false);
     vector<int>& vec = adj[i];
+    visited[i] = true;
     for(int j = 0; j < vec.size(); j++) {
-      if (i == j) continue; // skip checking self
-      if (acyclic_helper(adj, j, i, visited)) return true;
+      if (i == vec[j]) continue; // skip checking self
+      // std::cout << "vec[j]=" << vec[j] << ", i=" << i << std::endl;
+      if (acyclic_helper(adj, vec[j], i, visited)) return true;
     }
   }
   return false;
--- coursera/graphs/week02/homework/toposort/toposort.cpp
+++ coursera/graphs/week02/homework/toposort/toposort.cpp
@@ -3,16 +3,36 @@
 #include <vector>
 
 using std::vector;
+using std::swap;
 using std::pair;
 
-void dfs(vector<vector<int> > &adj, vector<int> &used, vector<int> &order, int x) {
-  //write your code here
-}     
+void dfs(vector<vector<int> > &adj, vector<bool> &used, vector<int>& order, int x) {
+  if (used[x]) return;
+
+  vector<int>& vec = adj[x];
+  bool isSink = true;
+  for(vector<int>::iterator it = vec.begin(); it != vec.end() ; it++){
+    if (used[*it]) continue;
+    dfs(adj, used, order, *it);  // dfs next vertex
+    if (used[*it] == false) isSink = false; // if after dfs , next vertex still not used, it's also set as not used
+  }
+  if (isSink) {
+    used[x] = 1;
+    order.push_back(x);
+  }
+}
 
 vector<int> toposort(vector<vector<int> > adj) {
-  vector<int> used(adj.size(), 0);
+  vector<bool> used(adj.size(), false);
   vector<int> order;
-  //write your code here
+  for(int i = 0; i < adj.size(); i++) {
+    dfs(adj, used, order, i);
+  }
+  size_t whole = order.size();
+  size_t half = whole/2;
+  for(size_t i = 0; i < half; i++) {
+    swap(order[i], order[whole-1-i]);
+  }
   return order;
 }
 
