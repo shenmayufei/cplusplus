2578b505cbbe0bd254569b16be157ccdb4e21282
oscarzhao <oscarzhaosl@gmail.com>
2016-11-15 00:48:16 +0800

    add dijkstra algorithm to solve shortest path problem

--- coursera/graphs/week04/homework/dijkstra/dijkstra.cpp
+++ coursera/graphs/week04/homework/dijkstra/dijkstra.cpp
@@ -7,9 +7,42 @@ using std::queue;
 using std::pair;
 using std::priority_queue;
 
+typedef pair<int, int> IntPair;
+class Greater
+{
+public:
+  bool operator() (const IntPair& lhs, const IntPair& rhs) const
+  {
+    return lhs.second > rhs.second;
+  }
+};
+
 int distance(vector<vector<int> > &adj, vector<vector<int> > &cost, int s, int t) {
-  //write your code her
-  return -1;
+  if (s == t) return t;
+
+  vector<int> dist(adj.size(), -1); // initialize dist
+  dist[s] = 0; 
+
+  priority_queue<IntPair> q; // store known distances
+  q.push(std::make_pair(s, dist[s]));
+
+  while(q.empty() == false) {
+    int idx = q.top().first;
+    // std::cout << "idx: " << idx << ", val: " << dist[idx] << std::endl;
+    q.pop();
+    vector<int>& vertices = adj[idx];
+    vector<int>& edges = cost[idx];
+    for(int i = 0; i < vertices.size() ; i++) {
+      int v = vertices[i];
+      int e = edges[i];
+      if (dist[v] < 0 || dist[v] > dist[idx] + e) {
+        dist[v] = dist[idx] + e;
+        q.push(std::make_pair(v, dist[v]));
+      }
+    }
+  }
+
+  return dist[t];
 }
 
 int main() {
