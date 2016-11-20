db8a54bab885a457e3a8686d63694d0d89679175
oscarzhao <oscarzhaosl@gmail.com>
2016-11-21 03:51:08 +0800

    add little code to clustering problem(graphs)

--- coursera/graphs/week05/homework/clustering/clustering.cpp
+++ coursera/graphs/week05/homework/clustering/clustering.cpp
@@ -11,6 +11,22 @@ using std::pair;
 
 
 double clustering(vector<int> x, vector<int> y, int k) {
+  int vSize = x.size();
+  if (vSize < k) return -1.0;
+  if (vSize == k) return 0.0; // check 
+
+  // initialize distances between any two points
+  vector< vector<double> > cost(vSize, vector<double>(vSize, 0.0));
+  for(int i = 0; i < vSize; i++) {
+    for(int j = i+1; j < vSize; j++) {
+      cost[i][j] = cost[j][i] = sqrt(pow(x[i] - x[j], 2) + pow(y[i] - y[j], 2));
+    }
+  }
+
+  // @TODO: implementing idea
+
+  vector<int> dset(vSize, 0);
+
   //write your code here
   return -1.;
 } 
