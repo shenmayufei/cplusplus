b3e486c4dcf5862b413cbd7c690e785c3f76df40
oscarzhao <oscarzhaosl@gmail.com>
2016-11-20 21:11:06 +0800

    add homework for week04

--- coursera/graphs/week04/homework/negative_cycle/negative_cycle.cpp
+++ coursera/graphs/week04/homework/negative_cycle/negative_cycle.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
 #include <vector>
+#include <limits>
 
 using std::vector;
 
 int negative_cycle(vector<vector<int> > &adj, vector<vector<int> > &cost) {
-  //write your code here
+  if (adj.size() == 0) return 0; 
+
+  vector<int> dist(adj.size(), std::numeric_limits<int>::max());
+  vector<int> prev(adj.size(), -1);
+  dist[0] = 0;  // use the first vertex as S
+  for(int i = 0; i < dist.size()-1; i++) {
+    for(int u = 0; u < adj.size();u++) {
+      if (dist[u] == std::numeric_limits<int>::max()) continue;
+      vector<int>& tmp = adj[u];
+      for (int k = 0; k < tmp.size(); k++) {
+        int v = tmp[k];
+        // std::cout << "round " << i+1 << ", u " << u << ", dist[u]:" << dist[u] << ", v:" << v << ", dist[v]:" << dist[v] << ", cost:" << cost[u][k] << std::endl;
+        if (dist[v] > dist[u] + cost[u][k])  {
+          dist[v] = dist[u] + cost[u][k];
+          prev[v] = u;
+        }
+        // std::cout << "round " << i+1 << ", u " << u << ", dist[u]:" << dist[u] << ", v:" << v << ", dist[v]:" << dist[v] << ", cost:" << cost[u][k] << std::endl << std::endl;
+      }
+    }
+  }
+
+  for(int u = 0; u < adj.size();u++) {
+    if (dist[u] == std::numeric_limits<int>::max()) continue;
+    vector<int>& tmp = adj[u];
+    for (int k = 0; k < tmp.size(); k++) {
+      int v = tmp[k];
+      // std::cout << "round " << adj.size() << ", u " << u << ", dist[u]:" << dist[u] << ", v:" << v << ", dist[v]:" << dist[v] << ", cost:" << cost[u][k] << std::endl;
+      if (dist[v] > dist[u] + cost[u][k]) return 1;
+    }
+  }
   return 0;
 }
 
--- coursera/graphs/week04/homework/shortest_paths/shortest_paths.cpp
+++ coursera/graphs/week04/homework/shortest_paths/shortest_paths.cpp
@@ -6,10 +6,70 @@
 using std::vector;
 using std::queue;
 using std::pair;
-using std::priority_queue;
 
-void shortest_paths(vector<vector<int> > &adj, vector<vector<int> > &cost, int s, vector<long long> &distance, vector<int> &reachable, vector<int> &shortest) {
-  //write your code here
+void shortest_paths(vector<vector<int> > &adj, vector<vector<int> > &cost, int s, vector<long long> &dist, vector<bool> &reachable, vector<int> &shortest) {
+  dist[s] = 0;
+  reachable[s] = 1;
+
+  // find all reachable ones
+  queue<int> rq;
+  rq.push(s);
+  while(rq.empty() == false) {
+    int t = rq.front();
+    rq.pop();
+    vector<int>& tmp = adj[t];
+    for(int i = 0; i < tmp.size() ; i++) {
+      int ele = tmp[i];
+      if (reachable[ele] == false) {
+        reachable[ele] = true;
+        rq.push(ele);
+        dist[ele] = dist[t] + cost[t][i];  // initialize dist
+      }
+    }
+  }
+
+  vector<int> prev(adj.size(), -1);
+  // run Bellman-Ford algorithm for reachable nodes only
+  for(int i = 1; i < dist.size(); i++) {
+    for(int u = 0; u < adj.size();u++) {
+      if (reachable[u] == false) continue;
+      vector<int>& tmp = adj[u];
+      for (int k = 0; k < tmp.size(); k++) {
+        int v = tmp[k];
+        // std::cout << "round " << i+1 << ", u " << u << ", dist[u]:" << dist[u] << ", v:" << v << ", dist[v]:" << dist[v] << ", cost:" << cost[u][k] << std::endl;
+        if (dist[v] > dist[u] + cost[u][k])  {
+          dist[v] = dist[u] + cost[u][k];
+          prev[v] = u;
+        }
+        // std::cout << "round " << i+1 << ", u " << u << ", dist[u]:" << dist[u] << ", v:" << v << ", dist[v]:" << dist[v] << ", cost:" << cost[u][k] << std::endl << std::endl;
+      }
+    }
+  }
+
+  for(int u = 0; u < adj.size();u++) {
+    if (reachable[u] == false) continue;
+    vector<int>& tmp = adj[u];
+    for (int k = 0; k < tmp.size(); k++) {
+      int v = tmp[k];
+      // std::cout << "round " << adj.size() << ", u " << u << ", dist[u]:" << dist[u] << ", v:" << v << ", dist[v]:" << dist[v] << ", cost:" << cost[u][k] << std::endl;
+      if (dist[v] > dist[u] + cost[u][k]) {
+        dist[v] = dist[u] + cost[u][k];
+        shortest[v] = 0; // there no shortest for those
+        prev[v] = u;
+      }
+    }
+  }
+
+  // reverse find negative cycles
+  for(int u = 0; u < adj.size(); u++) {
+    if (reachable[u] == false) continue;
+    if (shortest[u] == 1) continue;
+    int v = prev[u];
+    while(u != v) {
+      shortest[v] = 0;
+      v = prev[v];
+    }
+  }
 }
 
 int main() {
@@ -26,7 +86,7 @@ int main() {
   std::cin >> s;
   s--;
   vector<long long> distance(n, std::numeric_limits<long long>::max());
-  vector<int> reachable(n, 0);
+  vector<bool> reachable(n, false);
   vector<int> shortest(n, 1);
   shortest_paths(adj, cost, s, distance, reachable, shortest);
   for (int i = 0; i < n; i++) {
