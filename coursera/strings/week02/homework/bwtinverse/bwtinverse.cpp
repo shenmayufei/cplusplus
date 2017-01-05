02ed27e4ab1510daec0c8d63b703ed86d5c747a5
oscarzhao <oscarzhaosl@gmail.com>
2017-01-05 19:40:11 +0800

    inverse bwt string to original pattern

--- coursera/strings/week02/homework/bwtinverse/bwtinverse.cpp
+++ coursera/strings/week02/homework/bwtinverse/bwtinverse.cpp
@@ -9,10 +9,47 @@ using std::endl;
 using std::string;
 using std::vector;
 
-string InverseBWT(const string& bwt) {
-  string text = "";
+// struct Point is used to store letter information in a vector
+struct Point {
+  char elem;
+  int globalIndex;
+  Point(char elem, int gIndex) : elem(elem),globalIndex(gIndex) {};
+  friend bool operator<(const Point& x, const Point& y) {
+    if (x.elem < y.elem) return true;
+    else if (x.elem > y.elem) return false;
+    if (x.globalIndex < y.globalIndex) return true;
+    return false;
+  }
+};
 
-  // write your code here
+string InverseBWT(const string& bwt) {
+  vector<Point> firstCol;
+  firstCol.reserve(bwt.size());
+
+  for(int i = 0; i < bwt.size(); i++) {
+    firstCol.push_back(Point(bwt[i], i));
+  }
+
+  std::sort(firstCol.begin(), firstCol.end());
+
+  // print log 
+  // cout << "\nfirst col: \n";
+  // for(int i = 0; i < firstCol.size(); i++) {
+  //   cout << i << ": {" << firstCol[i].elem << ", " << firstCol[i].globalIndex << "}\n";
+  // }
+  // cout << endl;
+
+  string text;  // store the result
+  text.reserve(bwt.size());
+
+  int nextIndex = firstCol[0].globalIndex;
+
+  while(nextIndex != 0) {
+    const Point& p = firstCol[nextIndex];
+    text.push_back(p.elem);
+    nextIndex = p.globalIndex;
+  }
+  text.push_back('$');
 
   return text;
 }
