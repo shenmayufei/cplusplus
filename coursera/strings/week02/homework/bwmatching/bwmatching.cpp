7ebe128597f149c4332a641f555deb403b757e1e
oscarzhao <oscarzhaosl@gmail.com>
2017-01-08 22:13:41 +0800

    algorithm on strings: fix index problem on better bwmatching

--- coursera/strings/week02/homework/bwmatching/bwmatching.cpp
+++ coursera/strings/week02/homework/bwmatching/bwmatching.cpp
@@ -26,22 +26,28 @@ void PreprocessBWT(const string& bwt,
   // initialize occ_count_before
   string charSet = "$ACGT";
   for(int i = 0; i < charSet.size(); i++) {
-    occ_count_before[charSet[i]] = vector<int>(bwt.size(), 0);
+    occ_count_before[charSet[i]] = vector<int>(bwt.size()+1, 0);
   }
 
   char c = bwt[0];
-  vector<int>& vec = occ_count_before[c];
-  vec[0] = 1;
+  occ_count_before[c][1] = 1;
 
   for(int i = 1; i < bwt.size(); i++) {
     char c = bwt[i];
 
     // update occ_count_before according to bwt
     for(map<char, vector<int> >::iterator vIt = occ_count_before.begin(); vIt != occ_count_before.end(); vIt++) {
-      if (vIt->first == bwt[i]) vIt->second[i] = vIt->second[i-1] + 1;
-      else vIt->second[i] = vIt->second[i-1];
+      if (vIt->first == bwt[i]) vIt->second[i+1] = vIt->second[i] + 1;
+      else vIt->second[i+1] = vIt->second[i];
     }
   }
+  // for(map<char, vector<int> >::iterator vIt = occ_count_before.begin(); vIt != occ_count_before.end(); vIt++) {
+  //   std::cout << "char: " << vIt->first << ": ";
+  //   for(int i = 0; i < vIt->second.size(); i++) {
+  //     std::cout << vIt->second[i] << ", ";
+  //   }
+  //   std::cout << std::endl;
+  // }
 
   string sorted = bwt;
   std::sort(sorted.begin(), sorted.end(), std::less<char>());
@@ -49,6 +55,8 @@ void PreprocessBWT(const string& bwt,
     map<char, int>::const_iterator it = starts.find(sorted[i]);
     if (it == starts.end()) starts[sorted[i]] = i;
   }
+  // std::cout << "source: " << bwt << std::endl;
+  // std::cout << "sorted: " << sorted << std::endl << std::endl;
 }
 
 // Compute the number of occurrences of string pattern in the text
@@ -65,12 +73,15 @@ int CountOccurrences(const string& pattern,
     if (ptIndex < 0) return bottom - top + 1;
 
     char lastLetter = pattern[ptIndex--];
+    // std::cout << "pattern:" << pattern << ", index:" << ptIndex << std::endl;
     map<char, int>::const_iterator firstOccIt = starts.find(lastLetter);
     if (firstOccIt == starts.end()) return 0;
     map<char, vector<int> >::const_iterator vecIt = occ_count_before.find(lastLetter);
     const vector<int>& vec = vecIt->second;
+    // std::cout << "top :" << top << ", bottom:" << bottom << ", first occur:" << firstOccIt->second << ", count[top]:" << vec[top] << ", count[bottom]:" << vec[bottom] << std::endl;
     top = firstOccIt->second + vec[top];
     bottom = firstOccIt->second + vec[bottom+1] - 1;
+    // std::cout << "top: " << top << ", bottom: " << bottom << std::endl;
   }
   return 0;
 }
