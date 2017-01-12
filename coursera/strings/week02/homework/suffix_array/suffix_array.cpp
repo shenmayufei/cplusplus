07a70def36900a7e4df26b7fa7ce7500f7f54a01
oscarzhao <oscarzhaosl@gmail.com>
2017-01-12 23:45:22 +0800

    previous homework

--- coursera/strings/week02/homework/suffix_array/suffix_array.cpp
+++ coursera/strings/week02/homework/suffix_array/suffix_array.cpp
@@ -12,14 +12,33 @@ using std::pair;
 using std::string;
 using std::vector;
 
+struct Rotation {
+  string str;
+  int start;
+  Rotation(const string& src, int rotate): start(rotate) {
+    str = src.substr(rotate, string::npos) + src.substr(0, rotate);
+  }
+  friend bool operator<(const Rotation& r1, const Rotation& r2) {
+    return r1.str < r2.str;
+  }
+};
+
 // Build suffix array of the string text and
 // return a vector result of the same length as the text
 // such that the value result[i] is the index (0-based)
 // in text where the i-th lexicographically smallest
 // suffix of text starts.
 vector<int> BuildSuffixArray(const string& text) {
+  vector<Rotation> rotations;
+  rotations.reserve(text.size());
+  for(int i = 0; i < text.size(); i++) {
+    rotations.push_back(Rotation(text, i));
+  }
+  std::stable_sort(rotations.begin(), rotations.end());
   vector<int> result;
-  // Implement this function yourself
+  for(int i = 0; i < rotations.size(); i++) {
+    result.push_back(rotations[i].start);
+  }
   return result;
 }
 
