497f0e6380bd751c6b47f81513457dece3e22c23
oscarzhao <oscarzhaosl@gmail.com>
2017-04-30 14:30:18 +0800

    use a more memory saving way to do breadth first search

--- coursera/advanced_algorithms/week01/Programming-Assignment-1/evacuation/evacuation.cpp
+++ coursera/advanced_algorithms/week01/Programming-Assignment-1/evacuation/evacuation.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <vector>
-#include <utility>
+#include <queue>
+#include <limits>
+#include <algorithm>
 
 using std::vector;
-using std::pair;
-
-struct Node {
-    size_t edgeID, prev;
-};
+using std::queue;
+using std::numeric_limits;
 
 /* This class implements a bit unusual scheme for storing edges of the graph,
  * in order to retrieve the backward edge for a given edge quickly. */
@@ -77,59 +76,40 @@ FlowGraph read_data() {
 
 // dfs do a breadth-first search to FlowGraph graph, from vertex "from", to vertex
 // "to", if found vertext to, return flow
-int bfs(FlowGraph& graph, vector<size_t>& paths, int to) {
-    int flow = 0;
-    vector<Node> nodes;
+int bfs(FlowGraph& graph, vector<int>& paths, int from, int to) {
+    queue<size_t> vertexIDs;
     vector<bool> visited(graph.size(), false);
-    const vector<size_t> edgeIDs = graph.get_ids(0);
-    visited[0] = true;
-    for(vector<size_t>::const_iterator it = edgeIDs.begin(); it != edgeIDs.end(); it++) {
-        const Edge& ed = graph.get_edge(*it);
-        if (ed.capacity == 0) continue;
-        nodes.push_back(Node{*it, 0})
-        if (ed.to == to) {
-            paths.push_back(*it); // already included edgeIDs
-            flow = ed.capacity;
-            break;
-        }
-    }
-    if (flow > 0) {
-        return flow;
-    }
-
-    size_t nodeSize = nodes.size();
-    size_t idx = 0;
-    while(idx < nodeSize){
-        const Node& node = nodes[idx];
-        const Edge& ed = graph.get_edge(node.edgeID);
-        if (visited[ed.to] == true) continue; // already visited, skip
-        visited[ed.to] = true;
-        const vector<size_t> edgeIDs = graph.get_ids(ed.to);
-        for(vector<size_it>::const_iterator it = edgeIDs.begin(); it != edgeIDs.end(); it++) {
-            const Edge& nextEd = graph.get_edge(*it);
-            if (nextEd.capacity == 0) continue; // zero capacity, skip
-            nodes.push_back(Node{*it, idx});
-            if (nextEd.to == to) {
-                flow = nextEd.capacity;
+    vector<int> parentEdges(graph.size(), -1);
+    vertexIDs.push(from);
+    bool found = false;
+    while(!vertexIDs.empty() && !found) {
+        size_t vertexID = vertextIDs.front();
+        vertexIDs.pop();
+        visited[vertexID] = true;
+        const vector<size_t> edgeIDs = graph.get_ids(vertexID);
+        for(vector<size_t>::const_iterator it = edgeIDs.begin(); it != edgeIDs.end(); it++) {
+            const Edge& ed = graph.get_edge(*it);
+            if (ed.capacity == 0) continue;
+            parentEdges[ed.to] = *it;  // set parentEdges[v] as edge ID which lead to it
+            if (ed.to == to) {
+                found = true;
                 break;
             }
+            vertexIDs.push(ed.to);
         }
-        if (flow > 0) break;
-        idx++;
-        nodeSize = nodes.size();
     }
-    if (flow == 0) return flow;
-    idx = nodes.size()-1;
-    while(true){
-        const Node& node = nodes[idx];
-        size_t edgeID = node.edgeID;
-        paths.push_back(edgeID);
-        const Edge& ed = graph.get_edge(node.edgeID);
-        if (flow == 0) flow = ed.capacity;
-        else if (flow > ed.capacity) flow = ed.capacity;
-        if (idx==node.prev) break;
-        idx = node.prev;
+
+    if (!found) return 0; // cannot find a valid flow
+    int flow = numeric_limit<int>::max();
+    while(to != from) {
+        size_t edgeID = parentEdges[to];
+        const Edge& ed = graph.get_edge(edgeID);
+        if (flow > ed.capacity) flow = ed.capacity;
+        path.push_back(edgeID);
+        to = ed.from;
     }
+    
+    std::reverse(path.begin(), path.end());
     return flow;
 }
 
