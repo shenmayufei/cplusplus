279e96fc5345ab2b2893da13658d108ef41844ef
oscarzhao <oscarzhaosl@gmail.com>
2017-04-30 13:25:18 +0800

    implement bfs in a memory-consuming way

--- coursera/advanced_algorithms/week01/Programming-Assignment-1/evacuation/evacuation.cpp
+++ coursera/advanced_algorithms/week01/Programming-Assignment-1/evacuation/evacuation.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <utility>
 
 using std::vector;
+using std::pair;
+
+struct Node {
+    size_t edgeID, prev;
+};
 
 /* This class implements a bit unusual scheme for storing edges of the graph,
  * in order to retrieve the backward edge for a given edge quickly. */
@@ -69,9 +75,77 @@ FlowGraph read_data() {
     return graph;
 }
 
+// dfs do a breadth-first search to FlowGraph graph, from vertex "from", to vertex
+// "to", if found vertext to, return flow
+int bfs(FlowGraph& graph, vector<size_t>& paths, int to) {
+    int flow = 0;
+    vector<Node> nodes;
+    vector<bool> visited(graph.size(), false);
+    const vector<size_t> edgeIDs = graph.get_ids(0);
+    visited[0] = true;
+    for(vector<size_t>::const_iterator it = edgeIDs.begin(); it != edgeIDs.end(); it++) {
+        const Edge& ed = graph.get_edge(*it);
+        if (ed.capacity == 0) continue;
+        nodes.push_back(Node{*it, 0})
+        if (ed.to == to) {
+            paths.push_back(*it); // already included edgeIDs
+            flow = ed.capacity;
+            break;
+        }
+    }
+    if (flow > 0) {
+        return flow;
+    }
+
+    size_t nodeSize = nodes.size();
+    size_t idx = 0;
+    while(idx < nodeSize){
+        const Node& node = nodes[idx];
+        const Edge& ed = graph.get_edge(node.edgeID);
+        if (visited[ed.to] == true) continue; // already visited, skip
+        visited[ed.to] = true;
+        const vector<size_t> edgeIDs = graph.get_ids(ed.to);
+        for(vector<size_it>::const_iterator it = edgeIDs.begin(); it != edgeIDs.end(); it++) {
+            const Edge& nextEd = graph.get_edge(*it);
+            if (nextEd.capacity == 0) continue; // zero capacity, skip
+            nodes.push_back(Node{*it, idx});
+            if (nextEd.to == to) {
+                flow = nextEd.capacity;
+                break;
+            }
+        }
+        if (flow > 0) break;
+        idx++;
+        nodeSize = nodes.size();
+    }
+    if (flow == 0) return flow;
+    idx = nodes.size()-1;
+    while(true){
+        const Node& node = nodes[idx];
+        size_t edgeID = node.edgeID;
+        paths.push_back(edgeID);
+        const Edge& ed = graph.get_edge(node.edgeID);
+        if (flow == 0) flow = ed.capacity;
+        else if (flow > ed.capacity) flow = ed.capacity;
+        if (idx==node.prev) break;
+        idx = node.prev;
+    }
+    return flow;
+}
+
 int max_flow(FlowGraph& graph, int from, int to) {
     int flow = 0;
-    /* your code goes here */
+
+    while(true){
+        // calculate this flow
+        vector<size_t> paths;
+        int thisFlow = bfs(graph, paths, to);
+        flow+= thisFlow;
+        // paths is the edges of path in reversed order, thisFlow is the max flow available
+        
+        // @todo: add the flow to Graph
+    }
+
     return flow;
 }
 
