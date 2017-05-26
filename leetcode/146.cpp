063c413225ac0c9b51629d928e8543b81bc1ed22
oscarzhao <oscarzhaosl@gmail.com>
2017-05-26 13:05:11 +0800

    Add LRU algorithm

--- leetcode/146.cpp
+++ leetcode/146.cpp
@@ -1,12 +1,14 @@
 /*
-    Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and set.
+    Design and implement a data structure for Least Recently Used (LRU) cache. 
+    It should support the following operations: get and put.
 
-    get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
-    set(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+    1. get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+    2. put(key, value) - put or insert the value if the key is not already present. 
+        When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
 */
 #include<iostream>
-#include<map>
 #include<list>
+#include<unordered_map>
 
 class Pair {
     public:
@@ -17,18 +19,16 @@ class Pair {
 
 class LRUCache{
 private:
-    
+    std::unordered_map<int, std::list<Pair>::iterator> cache;
     int cap;
-    
-public:
     std::list<Pair> datalist;
-    std::map<int, std::list<Pair>::iterator> cache;
+public:
     LRUCache(int capacity) {
         cap = capacity;
     }
     
     int get(int key) {
-        std::map<int, std::list<Pair>::iterator >::iterator it = cache.find(key);
+        std::unordered_map<int, std::list<Pair>::iterator >::iterator it = cache.find(key);
         if (it != cache.end()) {
             Pair record = *(it->second); // memory allocation(need to optimize)
             datalist.erase(it->second);
@@ -40,14 +40,12 @@ public:
         }
     }
     
-    void set(int key, int value) {
-        std::map<int, std::list<Pair>::iterator >::iterator it = cache.find(key);
+    void put(int key, int value) {
+        std::unordered_map<int, std::list<Pair>::iterator >::iterator it = cache.find(key);
         if (it != cache.end()){
-            std::cout << "＊＊＊ set(" << key << "," << value << "): " << it->first << ": "<< it->second->key << "--" << it->second->val << std::endl;
             datalist.erase(it->second);
         }
         if (datalist.size() == cap) {
-            std::cout << "llllll\n";
             Pair unused_rec = datalist.front();
             datalist.pop_front();
             cache.erase(unused_rec.key);
@@ -59,35 +57,17 @@ public:
     }
 };
 
-void printList(LRUCache &cache) {
-    std::list<Pair> &list = cache.datalist;
-    std::cout << "--------Cache keys: ";
-    for (std::map<int, std::list<Pair>::iterator>::iterator it = cache.cache.begin(); it != cache.cache.end(); it++) {
-        std::cout << it->first << ", ";
-    }
-    std::cout << "\nList: " << std::endl;
-    for (std::list<Pair>::iterator it = list.begin(); it != list.end(); it++) {
-        std::cout << "key: " << it->key << ", val:" << it->val << std::endl;;
-    }
-    std::cout << "List end----------" << std::endl;
-}
 int main() {
     LRUCache cache(2);
     std::cout << "get(2): "<< cache.get(2) << std::endl;
-    std::cout << "set(2,6)" << std::endl; 
-    cache.set(2,6);
-    printList(cache);
+    std::cout << "put(2,6)" << std::endl; 
+    cache.put(2,6);
     std::cout << "get(1): "<< cache.get(1) << std::endl; 
-    printList(cache);
-    std::cout << "set(1,5)" << std::endl; 
-    cache.set(1, 5);
-    printList(cache);
-    std::cout << "set(1,2)" << std::endl; 
-    cache.set(1, 2);
-    printList(cache);
+    std::cout << "put(1,5)" << std::endl; 
+    cache.put(1, 5);
+    std::cout << "put(1,2)" << std::endl; 
+    cache.put(1, 2);
     std::cout << "get(1): "<< cache.get(1) << std::endl; 
-    printList(cache);
     std::cout << "get(2): "<< cache.get(2) << std::endl; 
-    printList(cache);
     
 }
