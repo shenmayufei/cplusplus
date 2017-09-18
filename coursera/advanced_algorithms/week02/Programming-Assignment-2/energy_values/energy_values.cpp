1adcf479c1eadc03152f5f785cbb117782aa23f1
oscarzhao <oscarzhaosl@gmail.com>
2017-09-18 17:44:35 +0800

    solve the energy values problem

--- coursera/advanced_algorithms/week02/Programming-Assignment-2/energy_values/energy_values.cpp
+++ coursera/advanced_algorithms/week02/Programming-Assignment-2/energy_values/energy_values.cpp
@@ -42,20 +42,39 @@ Equation ReadEquation() {
     return Equation(a, b);
 }
 
+// SelectPivotElement selects the first free element
+// the pivot value could be zero (which must be the first position)
+// or non-zero value
+// actually, column is still the row number (whose value is often not zero)
+// the most ideal condition is row==column (whose value is not zero, probably one)
 Position SelectPivotElement(
   const Matrix &a, 
   std::vector <bool> &used_rows, 
   std::vector <bool> &used_columns) {
-    // This algorithm selects the first free element.
-    // You'll need to improve it to pass the problem.
     Position pivot_element(0, 0);
     while (used_rows[pivot_element.row])
         ++pivot_element.row;
     while (used_columns[pivot_element.column])
         ++pivot_element.column;
+
+    size_t m = used_rows.size();
+    size_t n = used_columns.size();
+    if (pivot_element.row == m || pivot_element.column == n)
+        return pivot_element;  // invalid position
+
+    size_t i = pivot_element.column;
+    while(i < n){
+        if (a[pivot_element.row][i] != 0) {
+            pivot_element.column = i;
+            return pivot_element;
+        }
+        i++;
+    }
     return pivot_element;
 }
 
+// After swapping, row==column
+// the first free element could probably NOT zero, but also possibly is zero
 void SwapLines(Matrix &a, Column &b, std::vector <bool> &used_rows, Position &pivot_element) {
     std::swap(a[pivot_element.column], a[pivot_element.row]);
     std::swap(b[pivot_element.column], b[pivot_element.row]);
@@ -64,7 +83,23 @@ void SwapLines(Matrix &a, Column &b, std::vector <bool> &used_rows, Position &pi
 }
 
 void ProcessPivotElement(Matrix &a, Column &b, const Position &pivot_element) {
-    // Write your code here
+    if (a[pivot_element.row][pivot_element.column] == 0) return;
+    size_t n = b.size();
+
+    // set this row pivot value as ONE
+    double baseVal = a[pivot_element.row][pivot_element.column];
+    for(size_t j = pivot_element.column; j< n; j++) a[pivot_element.row][j] /= baseVal;
+    b[pivot_element.row] /= baseVal;
+
+    // update other rows
+    for(size_t i = 0; i < n; i++){
+        if (i==pivot_element.row) continue;
+        if (a[i][pivot_element.column]==0) continue;
+        double multBy = a[i][pivot_element.column];
+        for(size_t j = pivot_element.column; j < n; j++)
+            a[i][j] -= multBy * a[pivot_element.row][j];
+        b[i] -= multBy * b[pivot_element.row];
+    }
 }
 
 void MarkPivotElementUsed(const Position &pivot_element, std::vector <bool> &used_rows, std::vector <bool> &used_columns) {
@@ -81,8 +116,16 @@ Column SolveEquation(Equation equation) {
     std::vector <bool> used_rows(size, false);
     for (int step = 0; step < size; ++step) {
         Position pivot_element = SelectPivotElement(a, used_rows, used_columns);
+        // std::cout << "row:" << pivot_element.row << ", column:" << pivot_element.column 
+        // << ", value:" << a[pivot_element.row][pivot_element.column] << ", b:" << b[pivot_element.row] <<  std::endl;
+        // std::cout << a[1][0] << ", " << a[1][1] << "=>" << b[1] << std::endl;
         SwapLines(a, b, used_rows, pivot_element);
         ProcessPivotElement(a, b, pivot_element);
+        // std::cout << "after process, row:" << pivot_element.row << ", column:" << pivot_element.column 
+        // << ", value:" << a[pivot_element.row][pivot_element.column] << ", b:" << b[pivot_element.row]<<  std::endl;
+        // std::cout << a[1][0] << ", " << a[1][1] << "=>" << b[1] << std::endl;
+
+        // std::cout << std::endl;
         MarkPivotElementUsed(pivot_element, used_rows, used_columns);
     }
 
