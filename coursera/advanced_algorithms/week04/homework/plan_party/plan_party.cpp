9f38878811d5a3ad71fae51fd00667379bba1ef6
oscarzhao <oscarzhaosl@gmail.com>
2018-09-01 06:45:01 -0700

    plan party: try coursera test

--- coursera/advanced_algorithms/week04/homework/plan_party/plan_party.cpp
+++ coursera/advanced_algorithms/week04/homework/plan_party/plan_party.cpp
@@ -37,7 +37,7 @@ void dfs(const Graph &tree, int vertex, std::vector<std::vector<int> >& cache, s
         cache[vertex][0] += std::max(cache[child][0], cache[child][1]);
         cache[vertex][1] += cache[child][0];
     }
-    std::cout << "cache[" << vertex << "] = {" << cache[vertex][0] << ", " << cache[vertex][1] << std::endl;
+    // std::cout << "cache[" << vertex << "] = {" << cache[vertex][0] << ", " << cache[vertex][1] << std::endl;
 }
 
 int MaxWeightIndependentTreeSubset(const Graph &tree) {
